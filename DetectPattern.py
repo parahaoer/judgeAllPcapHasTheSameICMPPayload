ddd8469141bca32140f2ba40dc06f6ab20cda13d
parahaoer <884101054@qq.com>
2020-11-06 14:13:38 +0800

    新增对负载长度小于time_bytes_len的数据包的处理

--- DetectPattern.py
+++ DetectPattern.py
@@ -2,14 +2,21 @@ from PcapAnalyzer import PcapAnalyzer
 
 
 def detectPattern(icmp_payload, time_bytes_len):
-    if(len(icmp_payload) <= time_bytes_len+2):
+
+    payload_len = len(icmp_payload)
+    if(payload_len >= time_bytes_len and payload_len <= time_bytes_len+2):
         return 0
-    i = time_bytes_len 
-    j = time_bytes_len + 1
-    while(j < len(icmp_payload) and icmp_payload[i] != icmp_payload[j]):
+
+    if payload_len < time_bytes_len:
+        i = 0
+        j = 1
+    else:
+        i = time_bytes_len
+        j = time_bytes_len + 1
+    while(j < payload_len and icmp_payload[i] != icmp_payload[j]):
         j = j + 1
     
-    if j < len(icmp_payload) and icmp_payload[i] == icmp_payload[j]:
+    if j < payload_len and icmp_payload[i] == icmp_payload[j]:
         p_len = j - i
     else:
         p_len = 0
@@ -18,7 +25,7 @@ def detectPattern(icmp_payload, time_bytes_len):
         return 0
     else:
 
-        while(j < len(icmp_payload)):
+        while(j < payload_len):
             if(icmp_payload[i] != icmp_payload[j]):
                 return 0
             
@@ -32,13 +39,18 @@ def getPattern(icmp_payload, time_bytes_len):
     p_len = detectPattern(icmp_payload, time_bytes_len)
 
     if(p_len != 0):
-        r = time_bytes_len % p_len
-        left = time_bytes_len
-        right = left + p_len
-        m = left + p_len - r
-
-        pattern = icmp_payload[m:right] + icmp_payload[left: m]
+        if(len(icmp_payload) > time_bytes_len):
+            r = time_bytes_len % p_len
+            left = time_bytes_len
+            right = left + p_len
+            m = left + p_len - r
+            pattern = icmp_payload[m:right] + icmp_payload[left: m]
+            
+        else:
+            pattern = icmp_payload[0: p_len]
+        
         return pattern.hex()
+
     return ""
 
 
@@ -46,7 +58,7 @@ if __name__ == "__main__":
     pcapAnalyzer = PcapAnalyzer()
     pcapAnalyzer.get_filelist('pcap_dir/linux32/Centos_5.5_32(2.100).pcapng')
     pcap_list = pcapAnalyzer.pcap_list 
-    ftxt = open('output/output.txt', 'a', encoding="utf-8")
+    ftxt = open('output/output.txt', 'w', encoding="utf-8")
     for pcap_j in range(0, len(pcap_list)):
         for icmp_i in range(0, len(pcap_list[pcap_j])):
             icmp_payload_j_i = pcap_list[pcap_j][icmp_i]
